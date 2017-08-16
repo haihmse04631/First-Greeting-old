1f2d3c94c4dd7191fb6acfd0e2500fad4dd02bd5
haihm <haihmse04631@fpt.edu.vn>
2017-08-16 16:43:53 +0700

    Remove build!

--- Sources/First_Greeting/app/src/main/java/com/example/haihm/first_greeting/MainActivity.java
+++ Sources/First_Greeting/app/src/main/java/com/example/haihm/first_greeting/MainActivity.java
@@ -4,33 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ImageView;
 
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton btnLoginFacebook, btnLoginGmail;
+    ImageButton btnLoginGmail;
 
+    LoginButton btnLoginFacebook;
+    CallbackManager callbackManager;
+    AccessToken accessToken;
+    String fbId, fbEmail, fbName, fbImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-
         AnhXa();
-
-        btnLoginFacebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FirstGreetingMain.class);
-                startActivity(intent);
-            }
-        });
+        checkFacebookLogin();
 
         btnLoginGmail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,10 +57,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void AnhXa(){
-        btnLoginFacebook = (ImageButton) findViewById(R.id.btnLoginWithFacebook);
+    void AnhXa() {
         btnLoginGmail = (ImageButton) findViewById(R.id.btnLoginWithGmail);
     }
 
+    //Check login already or not
+    void checkFacebookLogin() {
+        btnLoginFacebook = (LoginButton) findViewById(R.id.btnLoginFacebook);
+        callbackManager = CallbackManager.Factory.create();
+        if (com.facebook.AccessToken.getCurrentAccessToken() != null) {
+            Intent intent = new Intent(MainActivity.this, LoginWithFacebook.class);
+            startActivity(intent);
+        } else {
+            processLogin();
+        }
+    }
+
+    // Proccessing login by facebook account
+    public void processLogin() {
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        //Login successed. Move to LoginWithFacebook.class
+                        checkFacebookLogin();
+                        Intent intent = new Intent(MainActivity.this, LoginWithFacebook.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
--- Sources/First_Greeting/app/src/main/java/com/example/haihm/first_greeting/Profile.java
+++ Sources/First_Greeting/app/src/main/java/com/example/haihm/first_greeting/Profile.java
@@ -10,15 +10,25 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.login.LoginManager;
+import com.squareup.picasso.Picasso;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+
 /**
  * Created by haihm on 8/8/2017.
  */
 
 public class Profile extends Fragment {
 
-     Button btnLogOut;
-     TextView tvUserName;
-     ImageView imgCover, imgAvatar;
+    Button btnLogOut;
+    TextView tvUserName;
+    ImageView imgCover, imgAvatar;
+    Bundle bund;
+
+    public Profile(Bundle bund) {
+        this.bund = bund;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,10 +40,17 @@ public class Profile extends Fragment {
         imgAvatar = (ImageView) rootView.findViewById(R.id.imgAvatar);
         imgCover = (ImageView) rootView.findViewById(R.id.imgCover);
 
+        tvUserName.setText(bund.getString("fbName"));
+        String fbImage = bund.getString("fbImage");
+        Picasso.with(getApplicationContext()).load(fbImage).into(imgAvatar);
+        String fbCover = bund.getString("fbCover");
+        Picasso.with(getApplicationContext()).load(fbCover).into(imgCover);
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                LoginManager.getInstance().logOut();
+                // Edit here to move back the login screen
             }
         });
 
